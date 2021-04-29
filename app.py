d76b1157fdcd997b47efb9e41802ab1017687294
Utkarsh Kumar <utkarshk384@gmail.com>
2021-04-29 21:03:32 +0600

    Fixed alot alot of bugs

--- app.py
+++ app.py
@@ -1,53 +1,66 @@
-usr_choice = 0
-choice_str = """
-Choose an option from the list below:
-1 - Add
-2 - Subtract
-3 - Divide
-4 - Multiply
+from tkinter import *
+from tkinter import ttk
 
-Your option:"""
+app = Tk()
 
 
-def add(nums):
-    return x + y
+app.minsize(485, 300)
+app.title("Unit Converter")
+
+container = ttk.Frame(app)
+
+frame = ttk.Frame(container, borderwidth=5,
+                  relief="ridge", width=200, height=100)
+
+#### First Variable ####
+
+# Adding Label
+ttk.Label(app, text="First Input Field\n").grid(column=0, row=1)
+
+# Adding Entries
+first_var = StringVar()
+first_field = ttk.Entry(app, textvariable=first_var,
+                        width=20).grid(column=0, row=3)
+
+# Adding unit Combo box
+
+first_box = StringVar()
+first_box_field = ttk.Combobox(
+    app, textvariable=first_box, width=5).grid(column=1, row=3)
+first_box_field['values'] = ("mm", "cm", "km", "m")
 
 
-def subtract():
-    return x - y
+#### Second Variable ####
+
+# Adding Label
+ttk.Label(app, text="Second Input Field\n").grid(column=3, row=1)
+
+# Adding Entries
+second_var = StringVar()
+first_field = ttk.Entry(app, textvariable=second_var,
+                        width=20).grid(column=3, row=3)
 
 
-def divide():
-    return x / y
+# Label "TO"
+ttk.Label(app, text="to").grid(column=2, row=2)
 
 
-def multiply():
-    return x * y
+# Output Field
+ttk.Label(app, text="Result", width=0).grid(column=2, row=4)
+output_var = StringVar()
+output_field = ttk.Entry(app, state="readonly", textvariable=output_var, width=20).grid(
+    column=2, row=5)
+
+# Adding unit Combo box
+
+second_box = StringVar()
+second_box_field = ttk.Combobox(
+    app, textvariable=second_box, width=5).grid(column=4, row=3)
 
 
-def get_input():
-    try:
-        usr_val = int(input("Please enter the mathematical expression: "))
-        return usr_val
-    except ValueError:
-        print("Please enter a number as the input")
+# Buttons
 
+exit_btn = ttk.Button(app, text="exit").grid(column=1, row=7)
+convert_btn = ttk.Button(app, text="Convert").grid(column=2, row=7)
 
-def main():
-    while True:
-        try:
-            usr_choice = int(input(choice_str))
-
-            if(usr_choice > 4 or usr_choice < 1):
-                print("Please enter a valid option")
-            break
-        except ValueError:
-            print("Please enter a number as the input")
-
-    if(usr_choice == 1):
-        usr_nums = get_input()
-        print(usr_nums)
-
-
-if (__name__ == "__main__"):
-    main()
+app.mainloop()
